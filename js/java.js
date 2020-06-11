79ff0883a393a58b1af14c1bf8a2f93c5cff0ad3
ibra987654321 <ibragimmadiyarov@gmail.com>
2020-06-11 17:01:36 +0300

    11june

--- js/java.js
+++ js/java.js
@@ -15,3 +15,53 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+var modal2 = document.getElementById('myModal2');
+
+var btn2 = document.getElementById("myBtn2");
+
+var span2 = document.getElementsByClassName("close2")[0]; 
+btn2.onclick = function() {
+    modal2.style.display = "block";
+}
+span2.onclick = function() {
+    modal2.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+}
+function calculateTip() {
+    var billAmt = document.getElementById("billAmount").value;
+    var tipAmt = document.getElementById("tipPercent").value;
+    
+    if (billAmt === "" && tipAmt == 0){
+    alert("Вы не указали количество литров");
+    }
+    var totalTip = (billAmt * tipAmt);
+    totalTip = totalTip.toFixed(2)  
+    document.getElementById("tip").innerHTML = totalTip;
+  }
+  
+  document.getElementById("calculate").onclick = function() {
+    calculateTip();
+  };
+ 
+
+//   jfdgnjdfngjdnfg
+function calculateTip2() {
+    var billAmt2 = document.getElementById("billAmount2").value;
+    var tipAmt2 = document.getElementById("tipPercent2").value;
+    
+    if (billAmt2 === "" && tipAmt2 == 0){
+    alert("Вы не указали количество литров");
+    }
+    var totalTip2 = (billAmt2 * tipAmt2);
+    totalTip2 = totalTip2.toFixed(2)  
+    document.getElementById("tip2").innerHTML = totalTip2;
+}
+
+document.getElementById("calculate2").onclick = function() {
+    calculateTip2();
+};
